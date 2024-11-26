b67dc7faabcba7d3dba80dca2b528e483986679c
FirstEncounter3 <needenergy27@gmail.com>
2024-11-26 16:39:30 +0700

    fix async\await calls

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ app = FastAPI()
 url_collection = init_db()
 
 @app.post("/shorten", response_model=str)
-def create_short_url(
+async def create_short_url(
     url: OriginalUrl = Body(..., description="The original URL to shorten"),
     # utm_source: str = Query(None, description="The utm_source parameter"),
     # utm_medium: str = Query(None, description="The utm_medium parameter"),
     # utm_campaign: str = Query(None, description="The utm_campaign parameter"),
 ) -> str:
-    return create_object_in_db_mongo(
+    return await create_object_in_db_mongo(
         url,
         url_collection
         # utm_source=utm_source,
@@ -27,18 +27,18 @@ def create_short_url(
 
 
 @app.get("/{id}")
-def get_short_url(id: str = Path(..., description="The ID of the short URL")) -> RedirectResponse:
-    url_data = get_object_from_db_mongo(id, url_collection)
+async def get_short_url(id: str = Path(..., description="The ID of the short URL")) -> RedirectResponse:
+    url_data = await get_object_from_db_mongo(id, url_collection)
     url_data.clicks += 1
     return RedirectResponse(url=url_data.original_url)
 
 
 @app.get("/stats/{id}", response_model=RecordUrl)
-def get_stats_url(id: str = Path(..., description="The ID of the short URL")) -> RecordUrl:
-    return get_object_from_db_mongo(id, url_collection)
+async def get_stats_url(id: str = Path(..., description="The ID of the short URL")) -> RecordUrl:
+    return await get_object_from_db_mongo(id, url_collection)
 
 
 if DEBUG:
     @app.get("/urls/all", response_model=list[RecordUrl])
-    def get_all_urls() -> list[RecordUrl]:
-        return get_all_objects_from_db_mongo(url_collection)
+    async def get_all_urls() -> list[RecordUrl]:
+        return await get_all_objects_from_db_mongo(url_collection)

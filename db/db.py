5c1f195c2253f431a8e476a8bd32f59eaab02d9e
First_Encounter <needenergy27@gmail.com>
2024-11-28 19:28:29 +0700

    Validate record in db, increase clicks handler

--- db/db.py
+++ db/db.py
@@ -1,6 +1,7 @@
 import datetime
 
 from fastapi import HTTPException
+from pydantic import ValidationError
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from models.models import OriginalUrl, RecordUrl
@@ -20,15 +21,20 @@ async def create_object_in_db_mongo(url: OriginalUrl, url_collection) -> str:
     id = create_unique_id()
     created_at = datetime.datetime.now()
     short_url = f"{HOSTNAME}{id}"
+    url = str(url.url)
+    
+    try:
+        record = RecordUrl(
+            id=id,
+            original_url=url,
+            short_url=short_url,
+            created_at=created_at,
+            clicks=0,
+        )
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-    await url_collection.insert_one(
-        {
-            "id": id,
-            "original_url": url.url,
-            "short_url": short_url,
-            "created_at": created_at,
-        }
-    )
+    await url_collection.insert_one(record.model_dump())
 
     return short_url
 
@@ -42,49 +48,21 @@ async def get_object_from_db_mongo(id: str, url_collection) -> RecordUrl:
     raise HTTPException(status_code=404, detail="URL not found")
 
 
+async def increase_the_number_of_clicks(id: str, url_collection) -> RecordUrl:
+    url_data = await url_collection.find_one({"id": id})
+
+    if url_data:
+        try:
+            clicks = url_data["clicks"] + 1
+            await url_collection.update_one({"id": id}, {"$set": {"clicks": clicks}})
+
+            return RecordUrl(**url_data)
+        except ValidationError as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+    raise HTTPException(status_code=404, detail="URL not found")
+
+
 async def get_all_objects_from_db_mongo(url_collection) -> list[RecordUrl]:
-    urls = []
-
-    async for url_data in url_collection.find():
-        urls.append(RecordUrl(**url_data))
-
+    urls = [RecordUrl(**url_data) async for url_data in url_collection.find()]
     return urls
-
-
-# db_shorten_urls = {}
-
-
-# def create_object_in_db(
-#     url: OriginalUrl,
-#     utm_source: str = None,
-#     utm_medium: str = None,
-#     utm_campaign: str = None,
-# ) -> str:
-
-#     id = create_unique_id()
-#     created_at = datetime.datetime.now()
-#     short_url = f"{HOSTNAME}{id}"
-
-#     db_shorten_urls[id] = RecordUrl(
-#         id=id,
-#         original_url=url.url,
-#         short_url=short_url,
-#         created_at=created_at,
-#         utm_source=utm_source,
-#         utm_medium=utm_medium,
-#         utm_campaign=utm_campaign,
-#     )
-
-#     return short_url
-
-
-# def get_object_from_db(id: str) -> RecordUrl:
-#     if id in db_shorten_urls:
-#         return db_shorten_urls[id]
-
-#     raise HTTPException(status_code=404, detail="URL not found")
-
-
-# dev
-# def get_all_objects_from_db() -> list[RecordUrl]:
-#     return list(db_shorten_urls.values())

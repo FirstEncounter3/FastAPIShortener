37c5f5ecd93f2de9d5978f62312643ed8debb194
First_Encounter <needenergy27@gmail.com>
2024-11-28 19:34:27 +0700

    add small comments and fix mongo host url

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,8 @@
+import os
 # HOSTNAME example: "http://localhost:8000/" - necessarily end with "/"
+# MONGO_HOST example: "mongodb://myuser:mypass@db:27017/" - "db" is the name of the container
 # DEBUG example: True or False if you want to use debug mode
 
 HOSTNAME = "http://localhost:8000/"
+MONGO_HOST = f"mongodb://{os.getenv('MONGO_INITDB_ROOT_USERNAME')}:{os.getenv('MONGO_INITDB_ROOT_PASSWORD')}@db:27017/"
 DEBUG = True
